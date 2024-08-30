eec94c9e9f56f911aadb243e64ae8cad41589a8f
Bianca-Pasat <67271750+Bianca-Pasat@users.noreply.github.com>
2024-08-30 12:43:35 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MultiOmicsIntegrator'
+copyright = '2024, BAP'
+author = 'Bianca'
 
 release = '0.1'
 version = '0.1.0'
